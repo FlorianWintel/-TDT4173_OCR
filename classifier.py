6cf8a0f19c947548d3a9fc5fafd6b6e2d69862aa
Florian Wintel <florianwintel@googlemail.com>
2019-04-25 13:01:24 +0200

    Separated detector, preprocessing and classifier into individual files. Added central run file.

---
+++ classifier.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+import string
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+
+from preprocessing import load_data, histogram_scale, background_correction, feature_selection
+
+
+def plot_confusion_matrix(y_true, y_pred, classes, cm, normalize=False, title=None, cmap=plt.cm.Blues):
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+    
+    
+def get_accuracy_and_cm(y_p, y_t):
+    """
+    Calculates the accuracy and confusion matrix for the given predictions.
+    :param y_p: float array, predictions
+    :param y_t: float array, target
+    :returns: 
+    """
+    right = 0;
+    for i in range(len(y_t)):
+        if y_t[i] == y_p[i]:
+            right += 1
+    con = confusion_matrix(y_t, y_p)
+    return right/len(y_t), con
+ 
+    
+"""
+
+def init_transformation(X, Y):
+    X,Y = load_data()
+    X, tr1 = histogram_scale(X)
+    X = background_correction(X)
+    X, tr2, tr3 = feature_selection(X,Y,1)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20)
+    
+
+    
+def init_knn():
+    
+    
+    # k-NN
+    n_nei = 8
+    neigh = KNeighborsClassifier(n_neighbors=n_nei)
+    neigh.fit(X_train, y_train)
+    kNN_y_pred = neigh.predict(X_test)
+    
+    kNN_right, kNN_con = correct_class(kNN_y_pred, y_test)
+    print ('kNN')
+    print('correct classified: ' + str(kNN_right))
+    #print('confusion matrix: \n' + str(kNN_con))
+    plot_confusion_matrix(y_test, kNN_y_pred, classes=alphabet, cm=kNN_con, title='Confusion matrix, kNN')
+    plt.show()
+    return neigh
+
+"""
+
+
+def classify(crop, clf):
+    """
+    Applies classifier.
+    :param crop: PIL image, a slice of the input image
+    :param clf: list [tranformation, classifier]
+    :returns: predicted class as string, prediction score as a float
+    """
+    # Apply data tranformation
+    X = np.resize(np.array(crop),(1,20*20))
+    X = clf[0][0].transform(X)
+    X = background_correction(X)
+    X = clf[0][1].transform(X)
+    X = clf[0][2].transform(X)
+
+    prediction = string.ascii_lowercase[clf[1].predict(X)[0]]
+    predict_score = np.max(clf[1].predict_proba(X))
+    
+    return prediction, predict_score
+    
+    
+def init_svm():
+    """
+    Initializacion of svm classifier and data transformation.
+    :returns: list which includes [data transformation, classifier]
+    """
+    X,Y = load_data()
+    X, tr1 = histogram_scale(X)
+    X = background_correction(X)
+    X, tr2, tr3 = feature_selection(X,Y,1)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20)
+    
+    # SVM
+    clf_svm = svm.SVC(gamma='scale', probability=True, C=10)
+    clf_svm.fit(X_train, y_train)
+    SVM_y_pred = clf_svm.predict(X_test)
+    
+    SVM_right, SVM_con = get_accuracy_and_cm(SVM_y_pred,y_test)
+    print ('SVM')
+    print('correct classified: ' + str(SVM_right))
+    return [[tr1, tr2, tr3], clf_svm]
+
+
+def predict(image, boxes, window_size, clf):
+    """
+    Predictor, calls the classifier on each found letter.
+    :param image: input image as PIL image object
+    :param boxes: list of tuples (x_min <int>, y_min <int>, detection score <float>)
+    :param window_size: size of the sliding window, tuple of integers (x,y)
+    :param clf: list [tranformation, classifier]
+    :returns: list of tuples (x_min <int>, y_min <int>, class prediction <string>, prediction score <float>)
+    """
+    classified_boxes = []
+    for box in boxes:
+        x,y = box[0], box[1]
+        prediction, predict_score = classify(image.crop((x,y,x+window_size[0],y+window_size[1])),clf)
+        classified_boxes.append((x, y, prediction, predict_score))
+    return classified_boxes
---
+++ run.py
@@ -0,0 +1,55 @@
+from PIL import Image
+import argparse
+import matplotlib.pyplot as plt
+from detector import scan, nms
+from classifier import predict, init_svm
+
+
+def plot(image, classified_boxes, window_size):
+    """
+    Plots the bounding boxes with class label and prediction score on top of the original image.
+    :param image: input image as PIL image object 
+    :param classified_boxes: list of tuples (x_min <int>, y_min <int>, class prediction <string>, prediction score <float>)
+    """
+    fig1 = plt.figure(dpi=400)
+    ax1 = fig1.add_subplot(1,1,1) 
+    ax1.imshow(image, cmap=plt.cm.gray)
+    ax1.axis('off')
+    for box in classified_boxes:
+        x_min, y_min, x_max, y_max = box[0]-.5, box[1]-.5, box[0]+window_size[0]-.5, box[1]+window_size[1]-.5
+        prediction, predict_score = box[2], box[3]
+        ax1.text(x_min, y_min-3, "%s %d%%" % (prediction, predict_score*100), color="red", fontsize=3)
+        x = [x_max, x_max, x_min, x_min, x_max]
+        y = [y_max, y_min, y_min, y_max, y_max]
+        line, = ax1.plot(x,y,color="red")
+        line.set_linewidth(.5)
+    fig1.savefig("classification.png")
+    return
+ 
+
+def main():
+    # Initialization of classifier
+    clf = init_svm()
+    # Load the image
+    parser = argparse.ArgumentParser()                                               
+    parser.add_argument("--input", "-i", type=str, required=False)
+    link = parser.parse_args().input
+    if link:
+        image = Image.open(link)    
+    else:
+        image = Image.open("./dataset/detection-images/detection-2.jpg")
+    # Hyperparameters
+    window_size         = (20,20)
+    stride              = (1,1)
+    detect_score_threshold     = .85 # Minimum detection score to be considered a hit.
+    iou_threshold       = .1 # Too high --> false positives occur.
+    max_boxes           = 100
+    # Ru(i)n everything
+    boxes               = scan(image, window_size, stride, detect_score_threshold)
+    boxes               = nms(boxes, window_size, max_boxes, iou_threshold)
+    classified_boxes    = predict(image, boxes, window_size, clf)
+    plot(image, classified_boxes, window_size)
+
+
+if __name__== "__main__":
+    main()
